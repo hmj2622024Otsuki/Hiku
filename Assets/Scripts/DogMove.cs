19ed178426799efbec9a3bcab276c9c0ed068619
hmj2622024Otsuki <hmj2622024@stu.o-hara.ac.jp>
2026-07-16 12:23:17 +0900

    新規プロジェクト

---
+++ Assets/Scripts/DogMove.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DogMove : MonoBehaviour
+{
+	[SerializeField] float speed = 7.0f;
+	[SerializeField] float rotateSpeed = 250f;
+	Animator animator;
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 direction = Vector3.zero;
+		// è„
+		if (Keyboard.current.wKey.isPressed)
+		{
+			transform.position += transform.forward * speed * Time.deltaTime;
+		}
+		if (Keyboard.current.wKey.wasPressedThisFrame) { animator.SetBool("Run", true); }
+		if (Keyboard.current.wKey.wasReleasedThisFrame) { animator.SetBool("Run", false); }
+
+		// â∫
+		if (Keyboard.current.sKey.isPressed)
+		{
+			direction = Vector3.back;
+
+			if (direction != Vector3.zero)
+			{
+				//	Quaternion targetRotation = Quaternion.LookRotation(direction);
+				//	transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+			}
+			transform.position += -transform.forward * speed * Time.deltaTime;
+		}
+		if (Keyboard.current.sKey.wasPressedThisFrame) { animator.SetBool("Run", true); }
+		if (Keyboard.current.sKey.wasReleasedThisFrame) { animator.SetBool("Run", false); }
+
+		// ç∂
+		if (Keyboard.current.aKey.isPressed)
+		{
+			direction = Quaternion.Euler(0, -30, 0) * transform.forward;
+
+			if (direction != Vector3.zero)
+			{
+				Quaternion targetRotation = Quaternion.LookRotation(direction);
+				transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+			}
+		}
+
+		// âE
+		if (Keyboard.current.dKey.isPressed)
+		{
+			direction = Quaternion.Euler(0, 30, 0) * transform.forward;
+
+			if (direction != Vector3.zero)
+			{
+				Quaternion targetRotation = Quaternion.LookRotation(direction);
+				transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+			}
+		}
+	}
+}
